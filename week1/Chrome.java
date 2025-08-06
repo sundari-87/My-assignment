0fd755a5630bef6e46126c9fc79ba7fb8030e5dc
sundari-87 <sundari.ulaganathan@gmail.com>
2025-08-06 23:46:54 +0530

    Add files via upload
    day1 files uploaded

---
+++ week1/Chrome.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		
+	float version=91.0f;
+	String developer="Google";
+	boolean beta=false;
+	int year=2008;
+	char key='C';
+	
+	System.out.println("Version:"+version+"\nDeveloper:"+developer+"\nBeta:"+beta+"\nYear:"+year+"\nKey:"+key);
+	
+	
+	
+				
+	}
+
+}
---
+++ week1/FirstProgram.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class FirstProgram {
+	
+	// type main
+	public static void main(String[] args) {
+	System.out.println("Hello Test leaf");
+	}
+
+}
