991e1592d27999831ae1cbff490a38c124777dd4
sundari-87 <sundari.ulaganathan@gmail.com>
2025-08-19 20:06:48 +0530

    Add files via upload

---
+++ homework.day2.week3/JavaConnection.java
@@ -0,0 +1,40 @@
+package week3.day2.homework;
+
+public class JavaConnection extends MySqlConnection implements  DatabseConnection{
+	
+	public void operation()
+	{
+		System.out.println("All the operation executed");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JavaConnection ja=new JavaConnection();
+		ja.connect();
+		ja.disconnect();
+		ja.executeUpdate();
+		ja.operation();
+		ja.executeQuery();
+
+	}
+
+	@Override
+	public void connect() {
+		System.out.println("Java connected to the database");
+		
+	}
+
+	@Override
+	public void disconnect() {
+		System.out.println("Disconnected from the database");
+		
+	}
+
+	@Override
+	public void executeUpdate() {
+		// TODO Auto-generated method stub
+		System.out.println("Update value executed");
+		
+	}
+
+}
---
+++ homework.day2.week3/MySqlConnection.java
@@ -0,0 +1,10 @@
+package week3.day2.homework;
+
+public abstract class MySqlConnection implements DatabseConnection {
+	
+	public void executeQuery() {
+		System.out.println("Executed the Query");
+	}
+	
+
+	}
